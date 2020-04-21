98f101d79619a46037e7608d8e64267029213dd9
Armaan Lala <armaanlala@gmail.com>
2020-04-20 23:39:38 -0400

    Starting Bulma and installed vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">
+          <h1>Armaan Lala</h1>
+        </router-link>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" :class="{ 'is-active': showNav }" @click="showNav = !showNav">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showNav }">
+        <div class="navbar-start">
+
+        </div>
+
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">
+            Home
+          </router-link>
+          <router-link to="/about" class="navbar-item">
+           About
+          </router-link>
+          <router-link to="/projects" class="navbar-item">
+           Projects
+          </router-link>
+          <router-link to="/contact" class="navbar-item">
+           Contact
+          </router-link>
+       </div>
+     </div>
+   </nav>
+   <router-view />
+   <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Armaan Lala 2020
+      </p>
+      <p>
+        Armaan Lala 2020
+      </p>
+      <p>
+        Armaan Lala 2020
+      </p>
+    </div>
+  </footer>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    data() {
+      return{
+        showNav: false
+      }
+    },
+  };
 </script>
 
-<style>
+<style type="text/css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* padding-top: 40px; */
+  min-height: 100vh;
+  overflow: hidden;
+  display: block;
+  position: relative;
+  padding-bottom: 104px; 
 }
+footer {
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+ min-height: fit-content;
+}
+.footer {
+  padding:1.5rem!important;
+}
+</style>
+
+<style lang="sass">
+@import "~bulma/bulma.sass"
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="hero is-cover is-relative is-fullheight-with-navbar has-background-link">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-1">Armaan Lala</h1>
+                <h2 class="subtitle is-2">Software Devolper</h2>
+                <h2 class="subtitle is-3">Mathmatics and Computer Science Student at Georgia Institute of Technology</h2>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "home",
+};
+</script>
