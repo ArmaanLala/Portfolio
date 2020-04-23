39096103d118a231ce892334a700ead77114be0c
armaanlala <armaanlala@gmail.com>
2020-04-23 03:45:31 -0400

    Working on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,241 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <section class="hero is-medium is-link is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-2">
+            About Armaan
+          </h1>
+        </div>
+      </div>
+    </section>
+
+    <section class="section has-background-light">
+      <h2 class="title is-3 has-text-weight-medium has-text-centered">
+        About Me
+      </h2>
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-four-fifths">
+            <p>
+              I am currently a second year student at the Georgia Instutite of
+              Technology studying for a Bachelor's degree in Mathematics , as
+              well as Computer Science with a concentration on Systems and
+              Architecture along with Intelligence. In addition, I am pursuing a
+              Minor in Physics.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <h2 class="title is-3 has-text-weight-medium has-text-centered">
+        Experience
+      </h2>
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+              <h3 class="title is-4 has-text-weight-medium">
+                MPECT (2018)
+              </h3>
+              <p>
+                I worked as an assistant App Devolper for MPECT based in
+                downtown Atlanta. I helped to write the backend code for the
+                databse structures, along with implementing various UI elements.
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box">
+              <h3 class="title is-4 has-text-weight-medium">
+                The Coder School (2018-19)
+              </h3>
+              <p>
+                I worked as a Code Coach teaching kids ages 8-16 various
+                programming skills and languages. I specialized in teaching both
+                Scratch and Python.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <h2 class="title is-3 has-text-weight-medium has-text-centered e">
+        Languages and Technologies I am Skilled In
+      </h2>
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-fifth">
+            <div class="box">
+              <figure>
+                <svg viewBox="0 0 128 128">
+                  <path
+                    fill="#D26383"
+                    d="M115.4 30.7l-48.3-27.8c-.8-.5-1.9-.7-3.1-.7-1.2 0-2.3.3-3.1.7l-48 27.9c-1.7 1-2.9 3.5-2.9 5.4v55.7c0 1.1.2 2.4 1 3.5l106.8-62c-.6-1.2-1.5-2.1-2.4-2.7z"
+                  ></path>
+                  <path
+                    fill="#9C033A"
+                    d="M10.7 95.3c.5.8 1.2 1.5 1.9 1.9l48.2 27.9c.8.5 1.9.7 3.1.7 1.2 0 2.3-.3 3.1-.7l48-27.9c1.7-1 2.9-3.5 2.9-5.4v-55.7c0-.9-.1-1.9-.6-2.8l-106.6 62z"
+                  ></path>
+                  <path
+                    fill="#fff"
+                    d="M85.3 76.1c-4.2 7.4-12.2 12.4-21.3 12.4-13.5 0-24.5-11-24.5-24.5s11-24.5 24.5-24.5c9.1 0 17.1 5 21.3 12.5l13-7.5c-6.8-11.9-19.6-20-34.3-20-21.8 0-39.5 17.7-39.5 39.5s17.7 39.5 39.5 39.5c14.6 0 27.4-8 34.2-19.8l-12.9-7.6z"
+                  ></path>
+                  <path
+                    d="M82.1 61.8h5.2v-5.3h4.4v5.3h5.3v4.4h-5.3v5.2h-4.4v-5.2h-5.2v-4.4zM100.6 61.8h5.2v-5.3h4.4v5.3h5.3v4.4h-5.3v5.2h-4.4v-5.2h-5.2v-4.4z"
+                  ></path>
+                </svg>
+              </figure>
+              <h3
+                class="tech-box-title title is-4 has-text-weight-medium has-text-centered"
+              >
+                C++
+              </h3>
+              <p>
+                Linux Systems Programming all done in C++
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-fifth">
+            <div class="box">
+              <figure>
+                <svg viewBox="0 0 128 128">
+                  <path
+                    fill="#0074BD"
+                    d="M47.617 98.12s-4.767 2.774 3.397 3.71c9.892 1.13 14.947.968 25.845-1.092 0 0 2.871 1.795 6.873 3.351-24.439 10.47-55.308-.607-36.115-5.969zM44.629 84.455s-5.348 3.959 2.823 4.805c10.567 1.091 18.91 1.18 33.354-1.6 0 0 1.993 2.025 5.132 3.131-29.542 8.64-62.446.68-41.309-6.336z"
+                  ></path>
+                  <path
+                    fill="#EA2D2E"
+                    d="M69.802 61.271c6.025 6.935-1.58 13.17-1.58 13.17s15.289-7.891 8.269-17.777c-6.559-9.215-11.587-13.792 15.635-29.58 0 .001-42.731 10.67-22.324 34.187z"
+                  ></path>
+                  <path
+                    fill="#0074BD"
+                    d="M102.123 108.229s3.529 2.91-3.888 5.159c-14.102 4.272-58.706 5.56-71.094.171-4.451-1.938 3.899-4.625 6.526-5.192 2.739-.593 4.303-.485 4.303-.485-4.953-3.487-32.013 6.85-13.743 9.815 49.821 8.076 90.817-3.637 77.896-9.468zM49.912 70.294s-22.686 5.389-8.033 7.348c6.188.828 18.518.638 30.011-.326 9.39-.789 18.813-2.474 18.813-2.474s-3.308 1.419-5.704 3.053c-23.042 6.061-67.544 3.238-54.731-2.958 10.832-5.239 19.644-4.643 19.644-4.643zM90.609 93.041c23.421-12.167 12.591-23.86 5.032-22.285-1.848.385-2.677.72-2.677.72s.688-1.079 2-1.543c14.953-5.255 26.451 15.503-4.823 23.725 0-.002.359-.327.468-.617z"
+                  ></path>
+                  <path
+                    fill="#EA2D2E"
+                    d="M76.491 1.587s12.968 12.976-12.303 32.923c-20.266 16.006-4.621 25.13-.007 35.559-11.831-10.673-20.509-20.07-14.688-28.815 8.548-12.834 32.229-19.059 26.998-39.667z"
+                  ></path>
+                  <path
+                    fill="#0074BD"
+                    d="M52.214 126.021c22.476 1.437 57-.8 57.817-11.436 0 0-1.571 4.032-18.577 7.231-19.186 3.612-42.854 3.191-56.887.874 0 .001 2.875 2.381 17.647 3.331z"
+                  ></path>
+                </svg>
+              </figure>
+              <h3
+                class="tech-box-title title is-4 has-text-weight-medium has-text-centered"
+              >
+                Java
+              </h3>
+              <p>
+                Full production applications written in Vue
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-fifth">
+            <div class="box">
+              <figure>
+                <svg viewBox="0 0 128 128">
+                  <path
+                    d="m-2.3125e-8 8.9337 49.854 0.1586 14.167 24.47 14.432-24.47 49.547-0.1577-63.834 110.14zm126.98 0.6374-24.36 0.0207-38.476 66.052-38.453-66.052-24.749-0.0194 63.211 107.89zm-25.149-0.008-22.745 0.16758l-15.053 24.647-14.817-24.647-22.794-0.1679 37.731 64.476zM25.997 9.3929l23.002 0.0087M25.997 9.3929l23.002 0.0087"
+                    fill="none"
+                  ></path>
+                  <path
+                    d="m25.997 9.3929 23.002 0.0087l15.036 24.958 14.983-24.956 22.982-0.0057-37.85 65.655z"
+                    fill="#35495e"
+                  ></path>
+                  <path
+                    d="m0.91068 9.5686 25.066-0.1711 38.151 65.658 37.852-65.654 25.11 0.0263-62.966 108.06z"
+                    fill="#41b883"
+                  ></path>
+                </svg>
+              </figure>
+              <h3
+                class="tech-box-title title is-4 has-text-weight-medium has-text-centered"
+              >
+                Vue.js
+              </h3>
+              <p>
+                Full production applications written in Vue
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-fifth">
+            <div class="box">
+              <figure>
+                <svg viewBox="0 0 128 128">
+                  <path
+                    d="M126.67,98.44c-4.56,1.16-7.38.05-9.91-3.75-5.68-8.51-11.95-16.63-18-24.9-.78-1.07-1.59-2.12-2.6-3.45C89,76,81.85,85.2,75.14,94.77c-2.4,3.42-4.92,4.91-9.4,3.7L92.66,62.34,67.6,29.71c4.31-.84,7.29-.41,9.93,3.45,5.83,8.52,12.26,16.63,18.67,25.21C102.65,49.82,109,41.7,115,33.26c2.41-3.42,5-4.72,9.33-3.46-3.28,4.35-6.49,8.63-9.72,12.88-4.36,5.73-8.64,11.53-13.16,17.14-1.61,2-1.35,3.3.09,5.19C109.9,76,118.16,87.1,126.67,98.44Z"
+                  ></path>
+                  <path
+                    d="M1.33,61.74c.72-3.61,1.2-7.29,2.2-10.83,6-21.43,30.6-30.34,47.5-17.06C60.93,41.64,63.39,52.62,62.9,65H7.1c-.84,22.21,15.15,35.62,35.53,28.78,7.15-2.4,11.36-8,13.47-15,1.07-3.51,2.84-4.06,6.14-3.06-1.69,8.76-5.52,16.08-13.52,20.66-12,6.86-29.13,4.64-38.14-4.89C5.26,85.89,3,78.92,2,71.39c-.15-1.2-.46-2.38-.7-3.57Q1.33,64.78,1.33,61.74ZM7.2,60.25H57.63c-.33-16.06-10.33-27.47-24-27.57C18.63,32.56,7.85,43.7,7.2,60.25Z"
+                  ></path>
+                </svg>
+              </figure>
+              <h3
+                class="tech-box-title title is-4 has-text-weight-medium has-text-centered"
+              >
+                Express.js
+              </h3>
+              <p>
+                Proficient in ES6 and ES5 JS
+              </p>
+            </div>
+          </div>
+
+          <div class="column is-one-fifth">
+            <div class="box">
+              <figure>
+                <img src="../assets/python-original.svg" alt="" />
+              </figure>
+              <h3
+                class="tech-box-title title is-4 has-text-weight-medium has-text-centered"
+              >
+                Python
+              </h3>
+              <p>
+                Back-ends written using Node and Express
+              </p>
+            </div>
+          </div>
+
+        
+        </div>
+
+        <p
+          class="tech-box-title title is-3 has-text-weight-medium has-text-centered"
+        >
+          Other Technologies
+        </p>
+         <p
+          class="tech-box-title title is-4 has-text-weight-light has-text-centered"
+        >
+          Firebase • Mongodb • LaTeX • Matlab • Flutter
+        </p>
+      </div>
+    </section>
+   
   </div>
 </template>
+
+<style type="text/css">
+.box {
+  height: 100%;
+}
+.tech-box-title {
+  margin-top: 1.5rem;
+}
+</style>
+
+<script>
+export default {
+  name: "about",
+};
+</script>
